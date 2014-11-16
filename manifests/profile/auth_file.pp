48f91fd3c8ac7445d209599bae56ae713ed492e7
kashif74 <kashif.alig@gmail.com>
2014-11-16 18:39:46 +0000

    use auth file for https

--- manifests/profile/auth_file.pp
+++ manifests/profile/auth_file.pp
@@ -4,6 +4,6 @@ class openstack::profile::auth_file {
     admin_tenant    => 'admin',
     admin_password  => $::openstack::config::keystone_admin_password,
     region_name     => $::openstack::config::region,
-    controller_node => $::openstack::config::controller_address_api,
+    controller_node => $::openstack::config::keystone_server_fqdn,
   }
 }
