50a51f977cee87dee6034757dd00a4a30ed2a2cd
kashif74 <kashif.alig@gmail.com>
2014-11-19 12:15:35 +0000

    use keystone server fqdn for horizon

--- manifests/profile/horizon.pp
+++ manifests/profile/horizon.pp
@@ -4,6 +4,7 @@ class openstack::profile::horizon {
     fqdn            => [ '127.0.0.1', $::openstack::config::controller_address_api, $::fqdn,  hiera('openstack::controller::horizon_fqdn'), ],
     secret_key      => $::openstack::config::horizon_secret_key,
     cache_server_ip => $::openstack::config::controller_address_management,
+    keystone_url    => "${::openstack::config::keystone_auth_protocol}://${::openstack::config::keystone_server_fqdn}:5000/v2.0"
 
   }
 
